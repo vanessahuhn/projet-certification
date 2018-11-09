a17f22a6cf98c99e909690101e3a6cd31d62aa76
vanessahuhn <vanessa.huhn@gmail.com>
2018-11-09 17:14:44 +0100

    création du projet

---
+++ src/main/java/com/projet/ProjetCertificationApplication.java
@@ -0,0 +1,12 @@
+package com.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCertificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetCertificationApplication.class, args);
+	}
+}

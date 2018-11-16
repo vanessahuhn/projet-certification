9edee52d8e00cb3c3c863602fc66d588ea2e8a33
vanessahuhn <vanessa.huhn@gmail.com>
2018-11-16 15:09:23 +0100

    up UC US1 v1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,4 +7,14 @@
  * Author:  vanessa
  * Created: 14 nov. 2018
  */
-insert into match_foot (id, date_match, equipe_domicile, equipe_exterieur, buts_domicile, buts_exterieur, logo_domicile, logo_exterieur) values (HIBERNATE_SEQUENCE.nextval, '2018-11-18', 'Paris-SG', 'Olympique Lyonnais', 0, 4,'https://upload.wikimedia.org/wikipedia/fr/thumb/8/86/Paris_Saint-Germain_Logo.svg/1024px-Paris_Saint-Germain_Logo.svg.png', 'https://upload.wikimedia.org/wikipedia/fr/thumb/e/e2/Olympique_lyonnais_%28logo%29.svg/888px-Olympique_lyonnais_%28logo%29.svg.png');
+INSERT INTO `footdb`.`match_foot`
+(`id`,`buts_domicile`,
+`buts_exterieur`,
+`date_match`,
+`equipe_domicile`,
+`equipe_exterieur`,
+`logo_domicile`,
+`logo_exterieur`)
+VALUES
+( 1, 0, 4,'2018-11-18', 'Paris-SG', 'Olympique Lyonnais','https://upload.wikimedia.org/wikipedia/fr/thumb/8/86/Paris_Saint-Germain_Logo.svg/1024px-Paris_Saint-Germain_Logo.svg.png', 'https://upload.wikimedia.org/wikipedia/fr/thumb/e/e2/Olympique_lyonnais_%28logo%29.svg/888px-Olympique_lyonnais_%28logo%29.svg.png');
+
